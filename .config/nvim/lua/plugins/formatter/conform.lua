a39737015d92ff7ba4f99eb41e3501ba46f73537
Braily Guzman <brailyg@outlook.com>
2024-11-16 11:08:15 -0500

    Update nvim plugins and remove plugins for a more minimal experience

--- .config/nvim/lua/plugins/formatter/conform.lua
+++ .config/nvim/lua/plugins/formatter/conform.lua
@@ -1,39 +1,39 @@
 return {
-    'stevearc/conform.nvim',
-    lazy = true,
-    event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+	"stevearc/conform.nvim",
+	lazy = true,
+	event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
 
-    config = function()
-        local conform = require("conform")
+	config = function()
+		local conform = require("conform")
 
-        require("conform").setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
-                -- Conform will run multiple formatters sequentially
-                python = { "isort", "black" },
-                -- You can customize some of the format options for the filetype (:help conform.format)
-                rust = { "rustfmt", lsp_format = "fallback" },
-                -- Conform will run the first available formatter
-                javascript = { "prettierd", "prettier", stop_after_first = true },
-            },
-            format_on_save = {
-                -- These options will be passed to conform.format()
-                timeout_ms = 500,
-                lsp_format = "fallback",
-            },
-            formatters = {
-                prettier = {
-                    prepend_args = { "--single-quote", "--trailing-comma", "all", "--tab-width", "4" },
-                },
-            }
-        })
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				python = { "isort", "black" },
+				-- You can customize some of the format options for the filetype (:help conform.format)
+				rust = { "rustfmt", lsp_format = "fallback" },
+				-- Conform will run the first available formatter
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+			formatters = {
+				prettier = {
+					prepend_args = { "--single-quote", "--trailing-comma", "all", "--tab-width", "4" },
+				},
+			},
+		})
 
-        vim.keymap.set({ "n", "v" }, "<leader>cf", function()
-            conform.format({
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 1000,
-            })
-        end, { desc = "Format file or range (in visual mode)" })
-    end
+		vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
