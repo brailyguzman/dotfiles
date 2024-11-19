b5b86a3813e2fa94b4ceef5199ec0613e1a60194
Braily Guzman <brailyg@outlook.com>
2024-11-19 11:09:31 -0500

    Add markdown formatter

--- .config/nvim/lua/plugins/formatter/conform.lua
+++ .config/nvim/lua/plugins/formatter/conform.lua
@@ -15,6 +15,7 @@ return {
 				rust = { "rustfmt", lsp_format = "fallback" },
 				-- Conform will run the first available formatter
 				javascript = { "prettierd", "prettier", stop_after_first = true },
+				markdown = { "prettier" },
 			},
 			format_on_save = {
 				-- These options will be passed to conform.format()
