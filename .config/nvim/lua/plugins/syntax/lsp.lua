581c1368939ed6aa9349b5b8017e8b583fddcd13
Braily Guzman <brailyg@outlook.com>
2024-11-19 22:16:57 -0500

    Add spell config

--- .config/nvim/lua/plugins/syntax/lsp.lua
+++ .config/nvim/lua/plugins/syntax/lsp.lua
@@ -1,119 +1,116 @@
 return {
-	"VonHeikemen/lsp-zero.nvim",
-	branch = "v3.x",
-	dependencies = {
-		-- Mason
-		{ "williamboman/mason.nvim" },
-		{ "williamboman/mason-lspconfig.nvim" },
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
+    dependencies = {
+        -- Mason
+        { "williamboman/mason.nvim" },
+        { "williamboman/mason-lspconfig.nvim" },
 
-		-- Lsp
-		{ "neovim/nvim-lspconfig" },
-		{ "onsails/lspkind.nvim" },
+        -- Lsp
+        { "neovim/nvim-lspconfig" },
+        { "onsails/lspkind.nvim" },
 
-		-- Completion
-		{ "hrsh7th/cmp-nvim-lsp" },
-		{ "hrsh7th/cmp-nvim-lua" },
-		{ "hrsh7th/nvim-cmp" },
-		{ "hrsh7th/cmp-path" },
+        -- Completion
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "hrsh7th/cmp-nvim-lua" },
+        { "hrsh7th/nvim-cmp" },
+        { "hrsh7th/cmp-path" },
 
-		-- Snippets
-		{ "L3MON4D3/LuaSnip" },
-		{ "rafamadriz/friendly-snippets" },
-	},
-	config = function()
-		local lsp_zero = require("lsp-zero")
+        -- Snippets
+        { "L3MON4D3/LuaSnip" },
+        { "rafamadriz/friendly-snippets" },
+    },
+    config = function()
+        local lsp_zero = require("lsp-zero")
 
-		lsp_zero.on_attach(function(client, bufnr)
-			lsp_zero.default_keymaps({ buffer = bufnr })
-		end)
+        lsp_zero.on_attach(function(client, bufnr)
+            lsp_zero.default_keymaps({ buffer = bufnr })
+        end)
 
-		require("mason").setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+        require("mason").setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        })
 
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"jsonls",
-				"pyright",
-				"marksman",
-			},
-			handlers = {
-				lsp_zero.default_setup,
-			},
-		})
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "marksman",
+            },
+            handlers = {
+                lsp_zero.default_setup,
+            },
+        })
 
-		-- This prevents diagnostics from going away when we are on Insert mode.
-		vim.diagnostic.config({
-			virtual_text = true,
-			signs = true,
-			update_in_insert = true,
-			underline = true,
-			severity_sort = false,
-			float = true,
-		})
+        -- This prevents diagnostics from going away when we are on Insert mode.
+        vim.diagnostic.config({
+            virtual_text = true,
+            signs = true,
+            update_in_insert = true,
+            underline = true,
+            severity_sort = false,
+            float = true,
+        })
 
-		local cmp = require("cmp")
-		local cmp_action = require("lsp-zero").cmp_action()
-		local lspkind = require("lspkind")
+        local cmp = require("cmp")
+        local cmp_action = require("lsp-zero").cmp_action()
+        local lspkind = require("lspkind")
 
-		require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_vscode").lazy_load()
 
-		cmp.setup({
-			window = {
-				-- completion = {
-				--     border = 'rounded',
-				-- },
-				-- documentation = {
-				--     border = 'rounded',
-				-- }
-			},
-			formatting = {
-				format = lspkind.cmp_format({
-					mode = "symbol",
-					maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-					-- can also be a function to dynamically calculate max width such as
-					-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-					ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-					show_labelDetails = false, -- show labelDetails in menu. Disabled by default
-					-- The function below will be called before any actual modifications from lspkind
-					-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-					before = function(entry, vim_item)
-						return vim_item
-					end,
-				}),
-			},
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "nvim_lua" },
-				{ name = "luasnip" },
-				{ name = "buffer" },
-				{ name = "path" },
-			},
-			-- Set Preselected CMP to None
-			completion = {
-				completeopt = "noselect",
-			},
-			preselect = cmp.PreselectMode.None,
-			-- Mappings
-			mapping = cmp.mapping.preset.insert({
-				-- `Enter` key to confirm completion
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-				-- Ctrl+Space to trigger completion menu
-				["<C-Space>"] = cmp.mapping.complete(),
-				-- Navigate between snippet placeholder
-				["<C-f>"] = cmp_action.luasnip_jump_forward(),
-				["<C-b>"] = cmp_action.luasnip_jump_backward(),
-				-- Scroll up and down in the completion documentation
-				["<C-u>"] = cmp.mapping.scroll_docs(-4),
-				["<C-d>"] = cmp.mapping.scroll_docs(4),
-			}),
-		})
-	end,
+        cmp.setup({
+            window = {
+                completion = {
+                    border = "rounded",
+                },
+                documentation = {
+                    border = "rounded",
+                },
+            },
+            formatting = {
+                format = lspkind.cmp_format({
+                    mode = "symbol",
+                    maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                    -- can also be a function to dynamically calculate max width such as
+                    -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+                    ellipsis_char = "...",     -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                    show_labelDetails = false, -- show labelDetails in menu. Disabled by default
+                    -- The function below will be called before any actual modifications from lspkind
+                    -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+                    before = function(entry, vim_item)
+                        return vim_item
+                    end,
+                }),
+            },
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "nvim_lua" },
+                { name = "luasnip" },
+                { name = "buffer" },
+                { name = "path" },
+            },
+            -- Set Preselected CMP to None
+            completion = {
+                completeopt = "noselect",
+            },
+            preselect = cmp.PreselectMode.None,
+            -- Mappings
+            mapping = cmp.mapping.preset.insert({
+                -- `Enter` key to confirm completion
+                ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                -- Ctrl+Space to trigger completion menu
+                ["<C-Space>"] = cmp.mapping.complete(),
+                -- Navigate between snippet placeholder
+                ["<C-f>"] = cmp_action.luasnip_jump_forward(),
+                ["<C-b>"] = cmp_action.luasnip_jump_backward(),
+                -- Scroll up and down in the completion documentation
+                ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-d>"] = cmp.mapping.scroll_docs(4),
+            }),
+        })
+    end,
 }
