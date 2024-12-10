2fc7246550b46d485775fb4fd2b58735ee37dda9
Braily Guzman <brailyg@outlook.com>
2024-12-10 10:15:39 -0500

    Add nvm

--- .config/zsh/os/darwin.zsh
+++ .config/zsh/os/darwin.zsh
@@ -2,7 +2,9 @@
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # Node
-export PATH="/usr/local/opt/node@22/bin:$PATH"
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 # Auto suggestions
 source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
