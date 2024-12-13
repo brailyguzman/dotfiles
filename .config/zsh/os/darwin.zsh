da5bff72887f77faa2302b5e0bcb17449ff478c1
Braily Guzman <brailyg@outlook.com>
2024-12-13 18:54:36 -0500

    Add omz plugins

--- .config/zsh/os/darwin.zsh
+++ .config/zsh/os/darwin.zsh
@@ -1,10 +1,4 @@
-# Syntax highlighting
-source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
 # Node
 export NVM_DIR="$HOME/.nvm"
-  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-
-# Auto suggestions
-source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"                                       # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
