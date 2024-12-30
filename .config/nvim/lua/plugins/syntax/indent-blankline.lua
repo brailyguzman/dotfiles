8b6d884a384c2cd9c58986e221d5e21226f31cad
Braily Guzman <brailyg@outlook.com>
2024-12-30 01:28:13 -0500

    Update indent-blankline char and formatting

--- .config/nvim/lua/plugins/syntax/indent-blankline.lua
+++ .config/nvim/lua/plugins/syntax/indent-blankline.lua
@@ -1,33 +1,33 @@
 return {
-    'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
-    event = { 'BufReadPost', 'BufNewFile' },
-    opts = {
-        indent = {
-            char = '▏', -- VS Code uses a thin line character
-            tab_char = '▏', -- Same for tab indentation
-        },
-        scope = {
-            enabled = false, -- Scope highlighting is generally not used in VS Code
-        },
-        exclude = {
-            filetypes = {
-                'help',
-                'dashboard',
-                'neo-tree',
-                'Trouble',
-                'lazy',
-                'mason',
-                'notify',
-                'toggleterm',
-                'lazyterm',
-                'NvimTree', -- Exclude file explorer
-                'fugitive', -- Exclude Git status files
-            },
-        },
-    },
-    config = function(_, opts)
-        require('ibl').setup(opts)
-        vim.api.nvim_set_hl(0, 'IblIndent', { fg = '#45475a', nocombine = true })
-    end,
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	event = { "BufReadPost", "BufNewFile" },
+	opts = {
+		indent = {
+			char = "▏",
+		},
+		whitespace = { highlight = { "Whitespace", "NonText" } },
+		scope = {
+			enabled = false,
+		},
+		exclude = {
+			filetypes = {
+				"help",
+				"dashboard",
+				"neo-tree",
+				"Trouble",
+				"lazy",
+				"mason",
+				"notify",
+				"toggleterm",
+				"lazyterm",
+				"NvimTree", -- Exclude file explorer
+				"fugitive", -- Exclude Git status files
+			},
+		},
+	},
+	config = function(_, opts)
+		require("ibl").setup(opts)
+		-- vim.api.nvim_set_hl(0, "IblIndent", { fg = "#45475a", nocombine = true })
+	end,
 }
