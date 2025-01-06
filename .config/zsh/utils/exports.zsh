6b3f035aec03e94378ff8b46b317c56d03fd8a1b
Braily Guzman <brailyg@outlook.com>
2025-01-06 01:14:41 -0500

    Export fzf theme

--- .config/zsh/utils/exports.zsh
+++ .config/zsh/utils/exports.zsh
@@ -4,3 +4,11 @@ export VISUAL="nvim"
 
 # Starship
 export STARSHIP_CONFIG=~/.config/starship/starship.toml
+
+# FZF (Catppuccin Mocha)
+export FZF_DEFAULT_OPTS=" \
+--color=spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a \
+--multi"
