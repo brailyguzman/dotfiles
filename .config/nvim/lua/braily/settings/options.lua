66c6e9f5e7062c9a212fe96f7a47b82b5bd79e23
Braily Guzman <brailyg@outlook.com>
2025-01-06 01:14:09 -0500

    Add bullets plugin, update options and lazy plugins

--- .config/nvim/lua/braily/settings/options.lua
+++ .config/nvim/lua/braily/settings/options.lua
@@ -16,10 +16,9 @@ vim.opt.clipboard = "unnamedplus"
 -- Indenting
 vim.opt.autoindent = true
 vim.opt.smartindent = true
-vim.opt.cindent = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 
 -- Line numbers
@@ -64,3 +63,5 @@ vim.o.completeopt = "menu,menuone,noinsert,noselect"
 
 vim.opt.spell = true
 vim.opt.spelllang = "en_us"
+
+vim.g.markdown_recommended_style = 0
--- .config/nvim/lua/plugins/syntax/auto.lua
+++ .config/nvim/lua/plugins/syntax/auto.lua
@@ -1,26 +1,32 @@
 return {
-	{
-		"windwp/nvim-autopairs",
-		event = "insertenter",
-		config = true,
-	},
-	{
-		"windwp/nvim-ts-autotag",
-		event = "insertenter",
-		config = function()
-			require("nvim-ts-autotag").setup({
-				opts = {
-					-- Defaults
-					enable_close = true, -- Auto close tags
-					enable_rename = true, -- Auto rename pairs of tags
-					enable_close_on_slash = false, -- Auto close on trailing </
-				},
-				per_filetype = {
-					["html"] = {
-						enable_close = false,
-					},
-				},
-			})
-		end,
-	},
+    {
+        "windwp/nvim-autopairs",
+        event = "insertenter",
+        config = true,
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        event = "insertenter",
+        config = function()
+            require("nvim-ts-autotag").setup({
+                opts = {
+                    -- Defaults
+                    enable_close = true,           -- Auto close tags
+                    enable_rename = true,          -- Auto rename pairs of tags
+                    enable_close_on_slash = false, -- Auto close on trailing </
+                },
+                per_filetype = {
+                    ["html"] = {
+                        enable_close = false,
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "bullets-vim/bullets.vim",
+        config = function()
+            vim.g.bullets_delete_last_bullet_if_empty = 1
+        end,
+    }
 }
