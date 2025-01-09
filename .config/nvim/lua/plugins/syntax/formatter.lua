7bb98ca8407dc2a8ab14235b5fcf03bcaf404f02
Braily Guzman <brailyg@outlook.com>
2025-01-08 20:20:17 -0500

    Remove plugins and format

--- .config/nvim/lua/plugins/syntax/formatter.lua
+++ .config/nvim/lua/plugins/syntax/formatter.lua
@@ -1,35 +1,41 @@
 return {
-	"stevearc/conform.nvim",
-	dependencies = {
-		"williamboman/mason.nvim",
-		"zapling/mason-conform.nvim",
-	},
-	opts = {},
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				markdown = { "prettier" },
-				lua = { "stylua" },
-			},
+    "stevearc/conform.nvim",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "zapling/mason-conform.nvim",
+    },
+    opts = {},
+    config = function()
+        local conform = require("conform")
 
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
+        conform.setup({
+            formatters_by_ft = {
+                markdown = { "prettier" },
+                lua = { "stylua" },
+            },
 
-			formatters = {
-				prettier = {
-					prepend_args = {
-						"--prose-wrap=always",
-						"--end-of-line=lf",
-						"--print-width=80",
-					},
-				},
-			},
-		})
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
 
-		require("mason-conform").setup({
-			ignore_install = {},
-		})
-	end,
+            formatters = {
+                prettier = {
+                    prepend_args = { "--single-quote", "--trailing-comma", "all", "--tab-width", "4" },
+                },
+            },
+        })
+
+        require("mason-conform").setup({
+            ignore_install = {},
+        })
+
+        vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            })
+        end, { desc = "Format file or range (in visual mode)" })
+    end,
 }
--- .config/nvim/lua/plugins/ui/telescope.lua
+++ .config/nvim/lua/plugins/ui/telescope.lua
@@ -1,153 +1,29 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	config = function()
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        local builtin = require("telescope.builtin")
 
-		local telescope_image_preview = function()
-			local supported_images = { "svg", "png", "jpg", "jpeg", "gif", "webp", "avif" }
-			local from_entry = require("telescope.from_entry")
-			local Path = require("plenary.path")
-			local conf = require("telescope.config").values
-			local Previewers = require("telescope.previewers")
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+        vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
-			local previewers = require("telescope.previewers")
-			local image_api = require("image")
-
-			local is_image_preview = false
-			local image = nil
-			local last_file_path = ""
-
-			local is_supported_image = function(filepath)
-				local split_path = vim.split(filepath:lower(), ".", { plain = true })
-				local extension = split_path[#split_path]
-				return vim.tbl_contains(supported_images, extension)
-			end
-
-			local delete_image = function()
-				if not image then
-					return
-				end
-
-				image:clear()
-
-				is_image_preview = false
-			end
-
-			local create_image = function(filepath, winid, bufnr)
-				image = image_api.hijack_buffer(filepath, winid, bufnr)
-
-				if not image then
-					return
-				end
-
-				vim.schedule(function()
-					image:render()
-				end)
-
-				is_image_preview = true
-			end
-
-			local function defaulter(f, default_opts)
-				default_opts = default_opts or {}
-				return {
-					new = function(opts)
-						if conf.preview == false and not opts.preview then
-							return false
-						end
-						opts.preview = type(opts.preview) ~= "table" and {} or opts.preview
-						if type(conf.preview) == "table" then
-							for k, v in pairs(conf.preview) do
-								opts.preview[k] = vim.F.if_nil(opts.preview[k], v)
-							end
-						end
-						return f(opts)
-					end,
-					__call = function()
-						local ok, err = pcall(f(default_opts))
-						if not ok then
-							error(debug.traceback(err))
-						end
-					end,
-				}
-			end
-
-			-- NOTE: Add teardown to cat previewer to clear image when close Telescope
-			local file_previewer = defaulter(function(opts)
-				opts = opts or {}
-				local cwd = opts.cwd or vim.loop.cwd()
-				return Previewers.new_buffer_previewer({
-					title = "File Preview",
-					dyn_title = function(_, entry)
-						return Path:new(from_entry.path(entry, true)):normalize(cwd)
-					end,
-
-					get_buffer_by_name = function(_, entry)
-						return from_entry.path(entry, true)
-					end,
-
-					define_preview = function(self, entry, _)
-						local p = from_entry.path(entry, true)
-						if p == nil or p == "" then
-							return
-						end
-
-						conf.buffer_previewer_maker(p, self.state.bufnr, {
-							bufname = self.state.bufname,
-							winid = self.state.winid,
-							preview = opts.preview,
-						})
-					end,
-
-					teardown = function(_)
-						if is_image_preview then
-							delete_image()
-						end
-					end,
-				})
-			end, {})
-
-			local buffer_previewer_maker = function(filepath, bufnr, opts)
-				-- NOTE: Clear image when preview other file
-				if is_image_preview and last_file_path ~= filepath then
-					delete_image()
-				end
-
-				last_file_path = filepath
-
-				if is_supported_image(filepath) then
-					create_image(filepath, opts.winid, bufnr)
-				else
-					previewers.buffer_previewer_maker(filepath, bufnr, opts)
-				end
-			end
-
-			return { buffer_previewer_maker = buffer_previewer_maker, file_previewer = file_previewer.new }
-		end
-
-		local image_preview = telescope_image_preview()
-
-		require("telescope").setup({
-			defaults = {
-				file_ignore_patterns = { ".git/", "node_modules/" },
-				file_previewer = image_preview.file_previewer,
-				buffer_previewer_maker = image_preview.buffer_previewer_maker,
-			},
-			pickers = {
-				find_files = {
-					hidden = true,
-				},
-			},
-			extensions = {
-				file_browser = { hijack_netrw = true },
-			},
-		})
-	end,
+        require("telescope").setup({
+            defaults = {
+                file_ignore_patterns = { ".git/", "node_modules/" },
+            },
+            pickers = {
+                find_files = {
+                    hidden = true,
+                },
+            },
+            extensions = {
+                file_browser = { hijack_netrw = true },
+            },
+        })
+    end,
 }
