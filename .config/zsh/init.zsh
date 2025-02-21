e7d35ed89e41232f3dbf422756e59297d0ca3238
Braily Guzman <brailyg@outlook.com>
2025-02-21 18:09:44 -0500

    Remove nvm from zsh config

--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -12,7 +12,3 @@ plugins=(
 source $ZSH/oh-my-zsh.sh
 
 eval "$(starship init zsh)"
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
