b7c592bd69322099ccc8206867376bbed2c5d32e
Braily Guzman <brailyg@outlook.com>
2025-06-16 20:02:00 -0400

    Add brew installed zsh plugins source commands

--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -5,12 +5,14 @@ ZSH_THEME="robbyrussell"
 
 plugins=(
     git
-    zsh-syntax-highlighting
-    zsh-autosuggestions
-    zsh-autopair
 )
 
 source $ZSH/oh-my-zsh.sh
+
+# Plugins
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/local/share/zsh-autopair/autopair.zsh
 
 eval "$(starship init zsh)"
 
