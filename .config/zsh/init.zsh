24cbdf34965f8974aa4703b75130f2299584467c
Braily Guzman <brailyg@outlook.com>
2025-06-27 17:45:12 -0400

    Add Go path variables to init.zsh

--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -18,6 +18,11 @@ if [ "$OS_TYPE" = "Darwin" ]; then
   source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
   source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
   source /usr/local/share/zsh-autopair/autopair.zsh
+
+  export GOPATH=$HOME/go
+  export GOROOT=/usr/local/opt/go/libexec
+  export PATH=$PATH:$GOPATH/bin
+  export PATH=$PATH:$GOROOT/bin
 else
   # Linux-specific configurations
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
